79867a9401ea33c6556e287de4a69b92b9373520
ajay4515 <96809842+ajay4515@users.noreply.github.com>
2022-06-15 11:20:48 +0530

    Updates

---
+++ precache-manifest.d385f2e6c6ff2378b122a1f2efaefac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f191c7d0ba2402b02d7",
+    "url": "/my-app/static/css/main.c2ee68a8.chunk.css"
+  },
+  {
+    "revision": "7f191c7d0ba2402b02d7",
+    "url": "/my-app/static/js/main.7f191c7d.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/my-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "9f467190b4df4fe2318e",
+    "url": "/my-app/static/js/2.9f467190.chunk.js"
+  },
+  {
+    "revision": "ad6099f2e84041bd8e59",
+    "url": "/my-app/static/js/runtime~main.ad6099f2.js"
+  },
+  {
+    "revision": "92b1e3c9014e89e4fd5f6837169c6b6a",
+    "url": "/my-app/index.html"
+  }
+];
